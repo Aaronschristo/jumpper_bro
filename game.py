7dcb4134088235c073cc813498089f2122586364
Aaron S Christo <aronschristo@gmail.com>
2025-09-27 20:13:37 +0530

    Adding game assets

--- game.py
+++ game.py
@@ -6,10 +6,9 @@ pg.init()
 size = WIDTH, HEIGHT = 800, 400
 
 screen = pg.display.set_mode(size)
+pg.display.set_caption("Jumper Bro")
 
-run = True
-
-while run:
+while True:
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
 			pg.quit()
